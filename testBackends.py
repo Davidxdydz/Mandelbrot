691dc3670dca63e4e7bcef0bbebea3e85e063197
David Berger <david.berger@tum.de>
2021-01-19 16:24:25 +0100

    adjusted testing

--- testBackends.py
+++ testBackends.py
@@ -3,7 +3,7 @@ from util.constants import params
 
 forceCompile()
 testBackends(-0.5, 0, 1, 10, 10, windowTitle="testBackends", block=False)
-testBackends(*params[0], 100, 4/3, windowTitle="testBackends", block=False)
+testBackends(*params[0], 200, 4/3, windowTitle="testBackends", block=False)
 testBackends(*params[1], 50, windowTitle="testBackends", block=False)
 testBackends(*params[1], 1000, functions=['cuda',
                                           'tf', 'numba'], windowTitle="testBackends")
--- testCmaps.py
+++ testCmaps.py
@@ -2,7 +2,7 @@ from Mandelbrot import plotMandelbrot
 from util.constants import goodCmaps, allCmaps, params
 import random
 
-for i in range(len(goodCmaps)//4+1):
+for i in range(len(goodCmaps)//4):
     plotMandelbrot(*params[1], 1000, cmaps=goodCmaps[i*4:i*4+4],
                    subtitle=f"{i*4}-{i*4+4} from goodCmaps", windowTitle="testCmaps", block=False)
     plotMandelbrot(*params[0], 1000, cmaps=goodCmaps[i*4:i*4+4],
