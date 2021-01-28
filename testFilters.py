fixed overflow from log(0)

testFilters.py
@@ -4,7 +4,7 @@ import numpy as np
 
 filters = (
     None,
-    lambda x: np.log(x),
+    lambda x: np.log(np.e + x) - 1,
     lambda x: np.sqrt(x),
     lambda x: x**2
 )
@@ -13,4 +13,4 @@ cmaps = ['afmhot', 'jet', 'hot', 'cubehelix']
 plotMandelbrot(params[1], 500, cmaps=cmaps,
                filters=filters, windowTitle="testFilters",block = False)
 plotMandelbrot(params[0], 500, cmaps=cmaps,
-               filters=filters, windowTitle="testFilters")
+              filters=filters, windowTitle="testFilters")