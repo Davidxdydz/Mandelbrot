82e596f9363f8166db35bf6a4f838db480d1812e
David Berger <david.berger@tum.de>
2022-12-22 15:59:36 +0100

    Add iterations as command line argument

--- run.py
+++ run.py
@@ -4,9 +4,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--backend", default="")
-parser.add_argument("--height", default=500, type=int)
+parser.add_argument("--height", default=1000, type=int)
+parser.add_argument("--iterations", default=3000, type=int)
 args = parser.parse_args()
 
-mandelbrot = Mandelbrot(max_iterations=1000,
+mandelbrot = Mandelbrot(max_iterations=args.iterations,
                         backend=args.backend, height=args.height)
 mandelbrot.interactive()
